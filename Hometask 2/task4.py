23b508232f8800babc142a8c6083599220746fd0
DmitryKizaev <DmitryKizaev@users.noreply.github.com>
2020-01-31 00:33:38 +0300

    task 4

---
+++ Hometask 2/task4.py
@@ -0,0 +1,22 @@
+# Написать свою имплементацию функции range() из Python 2.x
+# (аналогично Python 3, только возвращает список).
+
+
+def myrange(start, stop, step):
+    l = []
+    i = start
+    while i <= stop:
+        l.append(i)
+        i += step
+    return l
+
+
+print("Example")
+print("Enter a,b,c to emulate range (a, b, c)")
+print("Enter a")
+a = int(input())
+print("Enter b")
+b = int(input())
+print("Enter c")
+c = int(input())
+print(myrange(a, b, c))
