e0f05d3d9e277c7a86d2c16c022982d0c5484baa
DmitryKizaev <DmitryKizaev@users.noreply.github.com>
2020-01-31 03:25:46 +0300

    more tests

--- Hometask 2/task4_extended.py
+++ Hometask 2/task4_extended.py
@@ -7,27 +7,39 @@ def myrange(*args):
         return myrange_defaults(*args)
     elif len(args) == 2:
         return myrange_defaults(args[1], args[0])
-    else:
+    elif len(args) == 3:
         return myrange_defaults(args[1], args[0], args[2])
+    else:
+        return "Wrong number of arguments"
 
 
 def myrange_defaults(stop, start=0, step=1):
-    if step == 0:
-        return 'Value Error'
     l = []
     i = start
-    if stop > start:
+    if stop > start and step > 0:
         while i < stop:
             l.append(i)
             i += step
-    else:
+    elif stop < start and step < 0:
         while i > stop:
             l.append(i)
-            i -= step
+            i += step
     return l
 
 
 print("Example")
+print("myrange gives", myrange(7))
+print("range gives   [0, 1, 2, 3, 4, 5, 6] \n")
+print("myrange gives", myrange(1, 8))
+print("range gives   [1, 2, 3, 4, 5, 6, 7] \n")
+print("myrange gives", myrange(0, 20, 5))
+print("range gives   [0, 5, 10, 15] \n")
+print("myrange gives", myrange(0, -7, -1))
+print("range gives   [0, -1, -2, -3, -4, -5, -6] \n")
+print("myrange gives", myrange(1, 0))
+print("range gives   [] \n")
+
+print("Сustom Example")
 print("Enter a,b,c to emulate range (a, b, c)")
 print("Enter a")
 a = int(input())
